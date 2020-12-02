d4fc248716d2b7f584dbc690d5bbe3a25af40856
vcarrere33 <vcarrere33@gmail.com>
2020-12-02 15:54:12 +0100

    [FEATURE] add readme doc

--- README.md
+++ README.md
@@ -1,17 +1,48 @@
 # node-mongo-dump
+> version 1 🚀
 
-> 
+**Menu**
 
-## Install
+* [👷‍♂️ How it's work](#-how-its-work])
+* [💻 NPM commands](#-npm-commands)
+* [📖 API](#-api])
+* [📝 Notes & questions](#-notes--questions)
+* [⏰ TODO](#-todo)
 
-## Changelog
+ ## 👷‍♂️ How it's work
 
-__0.0.0__
+This library is really simple. It's allows you to backup your Mongodb on a specific frequency using mongodump.
 
-- Initial release
+## 💻 NPM commands
 
-## License
+Install the library
+`npm i node-mongo-dump`
 
-Copyright (c) 2018
+Declare the function in the js file that start your server
+```
+const nodeMongo = require('node-mongo-dump');
 
-Licensed under the [MIT license](LICENSE).
+nodeMongo();
+```
+
+And then... That's it, you're Ready to go 🚀
+
+## 📖 API
+
+|Arguments|type|Default Value|Commentary|
+|----|----|----|-----|
+|frequency|string|`'0 0 * * *'`|How often you want to dump your database.|
+|nbSaved|number|`14`|The number of dumps you want to keep. If you reach the nbSaved, it will delete the oldest before saving a new one.|
+|host|string|`'localhost'`|Your mongodb host.|
+|port|string|`'27017'`|Your mongodb port.|
+|outPath|string|`'./../../dumps/'`|The directory where you want to save the dumps.|
+|dbName|string|`'db'`|Your database name|
+|withStdout|boolean|`false`|Variable to log the output of mongodump command|
+|withStderr|boolean|`false`|Variable to log the errors of mongodump command|
+|withClose|boolean|`false`|Variable to log the ouendtput of mongodump command|
+
+## 📝 Notes & questions
+
+
+
+## ⏰ TODO
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,84 @@
-const { spawn } = require('child_process');
-const cron = require('node-cron');
-const fs = require('fs');
+import { spawn } from 'child_process';
+import cron from 'node-cron';
+import fs from 'fs';
+import compressing from 'compressing';
 
-function dumpDb(
+const getDateWithTwoDigit = (date: number) => (`0${date}`).slice(-2);
+
+const getFileName = (dbName?: string) => {
+  const now = new Date();
+  const prefix = dbName ? `${dbName}-` : '';
+  return `${prefix}${now.getFullYear()}-${getDateWithTwoDigit(now.getMonth() + 1)}-${getDateWithTwoDigit(now.getDate() + 1)}-${getDateWithTwoDigit(now.getHours())}-${getDateWithTwoDigit(now.getMinutes())}-${getDateWithTwoDigit(now.getSeconds())}`
+}
+
+const dumpDb = (
   host: string,
   port: string,
   outPath: string,
   dbName: string,
-  nbSaved: number
-) {
-  return function() {
-    try {
-      const now = new Date();
-      const files = fs.readdirSync(outPath);
-      if (files.length > nbSaved) {
-        fs.rmSync(files[0]);
-      }
-      spawn(`mongodump --host="${host}" --port=${port} --out=${outPath}/${dbName}-${now} --db=${dbName}`);
-    } catch (error) {
-      console.log(error);
+  nbSaved: number,
+  withStdout: boolean,
+  withStderr: boolean,
+  withClose: boolean
+) => () => {
+  try {
+    if (!fs.existsSync(outPath)) {
+      fs.mkdirSync(outPath);
     }
+    const filter = new RegExp(`^${dbName}`);
+    const files = fs.readdirSync(outPath).filter((path: string) => filter.test(path));
+    if (files.length >= nbSaved) {
+      fs.rmSync(`${outPath}${files[0]}`);
+    }
+    const mongodump = spawn('mongodump', [
+      `--host="${host}"`,
+      `--port=${port}`,
+      `--out=${outPath}${getFileName(dbName)}`,
+      `--db=${dbName}`
+    ]);
+
+    if (withStdout) {
+      mongodump.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+      });
+    }
+
+    if (withStderr) {
+      mongodump.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+      });
+    }
+
+    mongodump.on('close', (code) => {
+      if (withClose) {
+        console.log(`child process exited with code ${code}`);
+      }
+      compressing.tar.compressDir(`${outPath}${getFileName(dbName)}`, `${outPath}${getFileName(dbName)}.tar`)
+        .then(() => {
+          compressing.gzip.compressFile(`${outPath}${getFileName(dbName)}.tar`, `${outPath}${getFileName(dbName)}.tar.gzip`)
+          .then(() => {
+            fs.rmSync(`${outPath}${getFileName(dbName)}.tar`);
+            fs.rmdir(`${outPath}${getFileName(dbName)}`, { recursive: true }, () => { });
+          })
+          .catch((e) => console.log(e));
+        })
+        .catch((e) => console.log(e));
+    });
+  } catch (error) {
+    console.log(error);
   }
 }
 
-export default function (
+module.exports = (
   frequency: string = '0 0 * * *',
+  nbSaved: number = 14,
   host: string = 'localhost',
   port: string = '27017',
   outPath: string = './../../dumps/',
   dbName: string = '',
-  nbSaved: number = 14
-) {
-  cron.schedule(frequency, dumpDb(host, port, outPath, dbName, nbSaved));
+  withStdout: boolean = false,
+  withStderr: boolean = false,
+  withClose: boolean = false
+) => {
+  cron.schedule(frequency, dumpDb(host, port, outPath, dbName, nbSaved, withStdout, withStderr, withClose));
 }
