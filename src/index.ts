6c6ecf4b290a32002da9177dcb5ddfcd90c76657
Matthieu Chane Wen Ching <matthieu.chanewenc@gmail.com>
2022-03-26 17:22:37 +0100

    [FIX] delete export from index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import * as cron from 'node-cron';
 import dumpDb from './dumpDb';
-
 import { Arguments } from './types';
 
 const nodeMongoDump = ({
@@ -27,5 +26,4 @@ const nodeMongoDump = ({
   cron.schedule(frequency, dumpDb({ host, port, outPath, dbName, nbSaved, withStdout, withStderr, withClose }));
 };
 
-export { default as dumpDb } from './dumpDb';
 export default nodeMongoDump;
